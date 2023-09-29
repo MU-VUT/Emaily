4afd0d720dcad6dd85a8ee22d8070a6d7d92d51f
Michal Urbanek <web.michalurbanek@gmail.com>
2023-09-29 10:15:46 +0200

    Path resolve update

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ if (process.env.NODE_ENV === "production") {
   // Expresss wil serve up the index.html file if it doesnt recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "builtinModules", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
