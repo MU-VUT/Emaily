cd2f35bb514648a1f1713f28a08fe7cf2f4abd7f
Michal Urbanek <web.michalurbanek@gmail.com>
2023-09-29 10:01:36 +0200

    Added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express"); //nebo import ES6
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -9,6 +10,8 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json()); // middleware for POST req for express
 
 app.use(
   cookieSession({
@@ -21,6 +24,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  //  Express will serve up prodtuction assets like our main.js File, or main.css file
+  app.use(express.static("client/build"));
+  // Expresss wil serve up the index.html file if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "builtinModules", "index.html")
+    );
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
